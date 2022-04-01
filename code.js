46bb41b03f26f4ebd99d379a015ceb5af7cdd31f
Moharram Moharrami <moharram.m98@gmail.com>
2022-04-01 13:44:16 +0430

    add weather icon to the dashboard

--- code.js
+++ code.js
@@ -3,7 +3,6 @@ let clockHolder = document.getElementById("time");
 fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
     .then (res => res.json())
     .then (data => {
-        console.log(data);
         document.body.style.backgroundImage = `url('${data.urls.full}')`;
         document.getElementById("author").textContent = `By: ${data.user.name}`;
     })
@@ -37,3 +36,20 @@ function displayClock() {
     clockHolder.textContent = time;
 }
 setInterval(displayClock, 1000);
+
+navigator.geolocation.getCurrentPosition(position => {
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
+        .then(res => {
+            if (!res.ok) {
+                throw Error("Weather data not available!");
+            }
+            return res.json();
+        })
+        .then(data => {
+            const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+            document.getElementById("weather").innerHTML = `
+                <img src=${iconUrl} />
+            `
+        })
+        .catch(err => console.log(err));
+}); 
